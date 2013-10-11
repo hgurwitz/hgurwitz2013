caa968c7f7a6198ec47ba9617b5a6bd246f2c6fe
hgurwitz <hadassahg@gmail.com>
2013-10-11 12:43:58 -0400

    Update SnakeMain.java

--- src/snake/SnakeMain.java
+++ src/snake/SnakeMain.java
@@ -5,33 +5,18 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import snake.computer_player.ComputerPlayerGameController;
-import snake.enums.GameType;
-import snake.multiplayer.MultiPlayerGameController;
-import snake.single_player.SinglePlayerGameController;
-
 public class SnakeMain extends JFrame {
 
 	protected static final long serialVersionUID = 1L;
 	protected JPanel panel;
 
-	public SnakeMain(GameType type) {
+	public SnakeMain(GameController controller) {
 		setTitle("Snake");
 		setLocationRelativeTo(null);
 		setSize(SnakeView.SIDELENGTH, SnakeView.SIDELENGTH);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		panel = new JPanel();
-		switch (type) {
-		case SINGLE_PLAYER:
-			panel.add(new SnakeView(new SinglePlayerGameController()));
-			break;
-		case MULTI_PLAYER:
-			panel.add(new SnakeView(new MultiPlayerGameController()));
-			break;
-		case COMPUTER_PLAYER:
-			panel.add(new SnakeView(new ComputerPlayerGameController()));
-			break;
-		}
+		panel.add(new SnakeView(controller));
 		panel.setPreferredSize(new Dimension(SnakeView.SIDELENGTH,
 				SnakeView.SIDELENGTH));
 		add(panel);
