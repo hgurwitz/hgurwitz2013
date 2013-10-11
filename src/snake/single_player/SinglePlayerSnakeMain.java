9a8e3a68cd68702f692a345a01dba3b18eb43464
hgurwitz <hadassahg@gmail.com>
2013-10-11 12:45:38 -0400

    Update SinglePlayerSnakeMain.java

--- src/snake/single_player/SinglePlayerSnakeMain.java
+++ src/snake/single_player/SinglePlayerSnakeMain.java
@@ -1,12 +1,11 @@
 package snake.single_player;
 
 import snake.SnakeMain;
-import snake.enums.GameType;
 
 public class SinglePlayerSnakeMain extends SnakeMain {
 
 	public SinglePlayerSnakeMain() {
-		super(GameType.SINGLE_PLAYER);
+		super(new SinglePlayerGameController());
 	}
 
 	public static void main(String[] args) {
