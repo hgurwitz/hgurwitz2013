7690c533d3ae2eb02eee6842a0e91ce5693d49f5
hgurwitz <hadassahg@gmail.com>
2013-10-11 12:45:06 -0400

    Update MultiPlayerSnakeMain.java

--- src/snake/multiplayer/MultiPlayerSnakeMain.java
+++ src/snake/multiplayer/MultiPlayerSnakeMain.java
@@ -1,12 +1,11 @@
 package snake.multiplayer;
 
 import snake.SnakeMain;
-import snake.enums.GameType;
 
 public class MultiPlayerSnakeMain extends SnakeMain {
 
 	public MultiPlayerSnakeMain() {
-		super(GameType.MULTI_PLAYER);
+		super(new MultiPlayerGameController());
 	}
 
 	public static void main(String[] args) {
